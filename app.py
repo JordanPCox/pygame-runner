d5b9aa42504c470d0220abb15ca547d6e912fb5b
Jordan P. Cox <cox.jordan.p@gmail.com>
2024-06-21 14:24:10 -0400

    Added font

--- app.py
+++ app.py
@@ -5,9 +5,13 @@ pygame.init()
 screen = pygame.display.set_mode((800,400))
 pygame.display.set_caption('Runner')
 clock = pygame.time.Clock()
+test_font = pygame.font.Font('fonts/Pixeltype.ttf', 50)
+# 2 arguments are font type, and font size
 
 sky_surface = pygame.image.load('graphics/sky.png')
 ground_surface = pygame.image.load('graphics/ground.png')
+text_surface = test_font.render('My game', False, 'Black')
+# 3 arguments: text, anti-aliasing, color
 
 while True:
     for event in pygame.event.get():
@@ -17,6 +21,7 @@ while True:
 
     screen.blit(sky_surface, (0, 0))
     screen.blit(ground_surface, (0, 300))
+    screen.blit(text_surface, (300, 50))
     # blit: block image transfer, aka putting one surface on another surface.
     pygame.display.update()
     clock.tick(60)
