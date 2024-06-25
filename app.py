4678e4b76356c3bf385b7d31cb2ecfeaefdbf6c2
Jordan P. Cox <cox.jordan.p@gmail.com>
2024-06-24 20:43:33 -0400

    The snail moves!

--- app.py
+++ app.py
@@ -13,6 +13,9 @@ ground_surface = pygame.image.load('graphics/ground.png')
 text_surface = test_font.render('My game', False, 'Black')
 # 3 arguments: text, anti-aliasing, color
 
+snail_surface = pygame.image.load('graphics/snail/snail1.png')
+snail_x_pos = 600
+
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -22,6 +25,8 @@ while True:
     screen.blit(sky_surface, (0, 0))
     screen.blit(ground_surface, (0, 300))
     screen.blit(text_surface, (300, 50))
+    snail_x_pos -= 4
+    screen.blit(snail_surface, (snail_x_pos, 250))
     # blit: block image transfer, aka putting one surface on another surface.
     pygame.display.update()
     clock.tick(60)
