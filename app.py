f25d1fca7c0c2c780e1bcbcce174b069516d404e
Jordan P. Cox <cox.jordan.p@gmail.com>
2024-06-26 18:39:27 -0400

    Built snail rectangle and added movement

--- app.py
+++ app.py
@@ -14,9 +14,9 @@ ground_surface = pygame.image.load('graphics/ground.png').convert()
 text_surface = test_font.render('My game', False, 'Black')
 # 3 arguments: text, anti-aliasing, color
 
-snail_surface = pygame.image.load('graphics/snail/snail1.png').convert_alpha()
+snail_surf = pygame.image.load('graphics/snail/snail1.png').convert_alpha()
 # adding _alpha after .convert to make the alpha (black/white) values transparent
-snail_x_pos = 600
+snail_rect = snail_surf.get_rect(bottomright = (600,300))
 
 player_surf = pygame.image.load('graphics/Player/player_walk_1.png').convert_alpha()
 player_rect = player_surf.get_rect(midbottom = (80,300))
@@ -32,9 +32,10 @@ while True:
     screen.blit(sky_surface, (0, 0))
     screen.blit(ground_surface, (0, 300))
     screen.blit(text_surface, (300, 50))
-    snail_x_pos -= 4
-    if snail_x_pos < -100: snail_x_pos = 800
-    screen.blit(snail_surface, (snail_x_pos, 250))
+
+    snail_rect.x -= 4
+    if snail_rect.right <= 0: snail_rect.left = 800
+    screen.blit(snail_surf,snail_rect)
     screen.blit(player_surf,player_rect)
 
     pygame.display.update()
