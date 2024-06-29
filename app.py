c2193434e425f9e445250005545a102ea69a3749
Jordan P. Cox <cox.jordan.p@gmail.com>
2024-06-29 09:34:27 -0400

    Used pygame.draw to create rectangles, added colors for score

--- app.py
+++ app.py
@@ -11,8 +11,10 @@ test_font = pygame.font.Font('fonts/Pixeltype.ttf', 50)
 sky_surface = pygame.image.load('graphics/sky.png').convert()
 # .convert converts the image into something pygame can use more efficiently.
 ground_surface = pygame.image.load('graphics/ground.png').convert()
-text_surface = test_font.render('My game', False, 'Black')
+score_surf = test_font.render('My game', False, (64,64,64))
 # 3 arguments: text, anti-aliasing, color
+
+score_rect = score_surf.get_rect(center = (400,50))
 
 snail_surf = pygame.image.load('graphics/snail/snail1.png').convert_alpha()
 # adding _alpha after .convert to make the alpha (black/white) values transparent
@@ -31,9 +33,14 @@ while True:
         #     print(event.pos)
 
     # blit: block image transfer, aka putting one surface on another surface.
-    screen.blit(sky_surface, (0, 0))
-    screen.blit(ground_surface, (0, 300))
-    screen.blit(text_surface, (300, 50))
+    screen.blit(sky_surface, (0,0))
+    screen.blit(ground_surface, (0,300))
+    pygame.draw.rect(screen,'#c0e8ec',score_rect)
+    # tells pygame we're going to draw something, then the type of shape, then put in 3 arguments: surface to draw on, color, and the rectangle we want to draw. You can add in a 4th and 5th argument for width and border radius.
+    pygame.draw.rect(screen,'#c0e8ec',score_rect,10)
+    pygame.draw.ellipse(screen,'Brown',pygame.Rect(50,200,100,100))
+    # Creating a rectangle within the method that takes 4 arguments: left, top, width, height
+    screen.blit(score_surf, (score_rect))
 
     snail_rect.x -= 4
     if snail_rect.right <= 0: snail_rect.left = 800
@@ -43,9 +50,9 @@ while True:
     # if player_rect.colliderect(snail_rect):
     #     print('collision')
 
-    mouse_pos = pygame.mouse.get_pos()
-    if player_rect.collidepoint((mouse_pos)):
-        print(pygame.mouse.get_pressed())
+    # mouse_pos = pygame.mouse.get_pos()
+    # if player_rect.collidepoint((mouse_pos)):
+    #     print(pygame.mouse.get_pressed())
 
     pygame.display.update()
     clock.tick(60)
