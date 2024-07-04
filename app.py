f2b536d95e6c2ffa41d063e9b7b75f50e80800c2
Jordan P. Cox <cox.jordan.p@gmail.com>
2024-07-04 09:59:53 -0400

    Score clock works and properly resets to 0 on fail state

--- app.py
+++ app.py
@@ -1,29 +1,30 @@
 import pygame
 from sys import exit
 
+def display_score():
+    current_time = pygame.time.get_ticks() - start_time # Gives time in milliseconds
+    score_surf = test_font.render(f'{current_time}',False,(64,64,64)) # It expects a string, so we change current_time to an integer with f'{}'
+    score_rect = score_surf.get_rect(center = (400,50))
+    screen.blit(score_surf,score_rect)
+
 pygame.init()
 screen = pygame.display.set_mode((800,400))
 pygame.display.set_caption('Runner')
 clock = pygame.time.Clock()
-test_font = pygame.font.Font('fonts/Pixeltype.ttf', 50)
+test_font = pygame.font.Font('fonts/Pixeltype.ttf', 50) # 2 arguments are font type, and font size
 game_active = True
-# 2 arguments are font type, and font size
+start_time = 0
 
-sky_surface = pygame.image.load('graphics/sky.png').convert()
-# .convert converts the image into something pygame can use more efficiently.
+sky_surface = pygame.image.load('graphics/sky.png').convert() # .convert converts the image into something pygame can use more efficiently.
 ground_surface = pygame.image.load('graphics/ground.png').convert()
-score_surf = test_font.render('My game', False, (64,64,64))
-# 3 arguments: text, anti-aliasing, color
+# score_surf = test_font.render('My game', False, (64,64,64)) # 3 arguments: text, anti-aliasing, color
+# score_rect = score_surf.get_rect(center = (400,50))
 
-score_rect = score_surf.get_rect(center = (400,50))
-
-snail_surf = pygame.image.load('graphics/snail/snail1.png').convert_alpha()
-# adding _alpha after .convert to make the alpha (black/white) values transparent
+snail_surf = pygame.image.load('graphics/snail/snail1.png').convert_alpha() # adding _alpha after .convert to make the alpha (black/white) values transparent
 snail_rect = snail_surf.get_rect(bottomright = (600,300))
 
 player_surf = pygame.image.load('graphics/Player/player_walk_1.png').convert_alpha()
-player_rect = player_surf.get_rect(midbottom = (80,300))
-# the .get_rect method draws a rectangle around the surface
+player_rect = player_surf.get_rect(midbottom = (80,300)) # the .get_rect method draws a rectangle around the surface
 player_gravity = 0
 
 while True:
@@ -44,15 +45,15 @@ while True:
             if event.type == pygame.KEYDOWN and event.key == pygame.K_SPACE:
                 game_active = True
                 snail_rect.left = 800
+                start_time = pygame.time.get_ticks()
 
-    # blit: block image transfer, aka putting one surface on another surface.
     if game_active:
-        screen.blit(sky_surface, (0,0))
+        screen.blit(sky_surface, (0,0)) # blit: block image transfer, aka putting one surface on another surface.
         screen.blit(ground_surface, (0,300))
-        pygame.draw.rect(screen,'#c0e8ec',score_rect)
-        # tells pygame we're going to draw something, then the type of shape, then put in 3 arguments: surface to draw on, color, and the rectangle we want to draw. You can add in a 4th and 5th argument for width and border radius.
-        pygame.draw.rect(screen,'#c0e8ec',score_rect,10)
-        screen.blit(score_surf, (score_rect))
+        # pygame.draw.rect(screen,'#c0e8ec',score_rect) # tells pygame we're going to draw something, then the type of shape, then put in 3 arguments: surface to draw on, color, and the rectangle we want to draw. You can add in a 4th and 5th argument for width and border radius.
+        # pygame.draw.rect(screen,'#c0e8ec',score_rect,10)
+        # screen.blit(score_surf, (score_rect))
+        display_score()
 
         snail_rect.x -= 4
         if snail_rect.right <= 0: snail_rect.left = 800
